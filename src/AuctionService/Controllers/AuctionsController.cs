7d9ddda734826fa57efbbe60ccf80c637515c063
Ayush Vyas <Ayush.Vyas@parentpay.com>
2024-04-13 17:21:14 +0530

    End of Section 3

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,14 +22,16 @@ public class AuctionsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
     {
-        var auctions = await _dbContext.Auctions
-                        .Include(x => x.Item)
-                        .OrderBy(x => x.Item.Make)
-                        .ToListAsync();
+        var query = _dbContext.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-        return _mapper.Map<List<AuctionDto>>(auctions);
+        if (!string.IsNullOrWhiteSpace(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     [HttpGet("{id}")]
@@ -89,7 +92,7 @@ public class AuctionsController : ControllerBase
         if (auction == null) return NotFound();
 
         // TODO: Check seller username   
-        
+
         _dbContext.Auctions.Remove(auction);
         var result = await _dbContext.SaveChangesAsync() > 0;
         if (!result) return BadRequest("Could not delete");
